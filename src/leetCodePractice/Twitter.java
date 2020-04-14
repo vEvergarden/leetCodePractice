8a9ec856e4877b3a768b9920a70a01ec6e6ffee1
vEvergarden <huangxinyi0528@qq.com>
2020-04-14 15:59:37 +0800

    intellj test

--- src/leetCodePractice/Twitter.java
+++ src/leetCodePractice/Twitter.java
@@ -1,6 +1,11 @@
 package leetCodePractice;
 
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Set;
 
 public class Twitter {
 	
@@ -23,16 +28,35 @@ public class Twitter {
 	
 	//推文表
     private Map<Integer, Tweet> twitter;
+    
+    //追星表
+    private Map<Integer, Set<Integer>> followings;
+    
+    //时间戳
+    private static int timestamp = 0;
+    
+    //k路合并
+    private static PriorityQueue<Tweet> maxHeap;
+    
+    
 	
 	/** Initialize your data structure here. */
     public Twitter() {
     	
-    	tweet = new Tweet();
+    	followings = new HashMap<>();
+    	twitter = new HashMap<>();
+    	maxHeap = new PriorityQueue<>((o1,o2)->o1.timestamp+o2.timestamp);
+    	
 
     }
     
     /** Compose a new tweet. */
     public void postTweet(int userId, int tweetId) {
+    	
+    	timestamp++;
+    	if(twitter.containsKey(userId)) {
+    		
+    	}
 
 
 
@@ -40,6 +64,9 @@ public class Twitter {
     
     /** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
     public List<Integer> getNewsFeed(int userId) {
+    	
+    	
+    	return null;
 
     }
     
